785d46d3d48327808ab04188abcaf23bfdc3e097
drakeet <drakeet.me@gmail.com>
2015-02-20 13:59:48 +0800

    Initial commit

---
+++ app/src/main/java/me/drakeet/materialsettingsactivitydemo/SettingsActivity.java
@@ -0,0 +1,44 @@
+package me.drakeet.materialsettingsactivitydemo;
+
+import android.annotation.TargetApi;
+import android.app.FragmentManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SettingsActivity extends ActionBarActivity {
+
+    private SettingsFragment mSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        if (savedInstanceState == null) {
+            mSettingsFragment = new SettingsFragment();
+            replaceFragment(R.id.settings_container, mSettingsFragment);
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void replaceFragment(int viewId, android.app.Fragment fragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(viewId, fragment).commit();
+    }
+
+    /**
+     * A placeholder fragment containing a settings view.
+     */
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+    
+}
